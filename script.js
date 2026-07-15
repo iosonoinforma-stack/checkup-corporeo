1b7d1530090caa66e8cb4f68052166a27a7731a0
iosonoinforma-stack <iosonoinforma@gmail.com>
2026-07-15 07:43:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const startButton = document.getElementById("startButton");
+
+    startButton.addEventListener("click", () => {
+
+        alert("Benvenuto nel Check-up Corporeo!\n\nNella prossima versione inizieremo con l'inserimento dei tuoi dati.");
+
+    });
+
+});
